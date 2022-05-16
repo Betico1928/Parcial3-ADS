b031d2a672b3f48c6aa6c814d86a562da821d792
Betico1928 <betoveluis@gmail.com>
2022-05-15 22:17:02 -0500

    Init
    - - Version Preliminar del Parcial - -
    
    - Interfaz gráfica implementada.

--- src/main/java/com/example/parcial_3ads/ControladorParcial.java
+++ src/main/java/com/example/parcial_3ads/ControladorParcial.java
@@ -1,16 +1,39 @@
+/**
+ * Sample Skeleton for 'Interfaz.fxml' Controller Class
+ */
+
 package com.example.parcial_3ads;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
 
-public class ControladorParcial
-{
-    @FXML
-    private Label welcomeText;
+public class ControladorParcial {
 
-    @FXML
-    protected void onHelloButtonClick()
-    {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    @FXML // fx:id="botonInscribirMateria"
+    private Button botonInscribirMateria; // Value injected by FXMLLoader
+
+    @FXML // fx:id="botonSeleccionNomina"
+    private Button botonSeleccionNomina; // Value injected by FXMLLoader
+
+    @FXML // fx:id="comboAsignarHoras"
+    private ComboBox<?> comboAsignarHoras; // Value injected by FXMLLoader
+
+    @FXML // fx:id="comboAsignarMateria"
+    private ComboBox<?> comboAsignarMateria; // Value injected by FXMLLoader
+
+    @FXML // fx:id="radioSeleccionMonitor"
+    private RadioButton radioSeleccionMonitor; // Value injected by FXMLLoader
+
+    @FXML // fx:id="radioSeleccionProfesor"
+    private RadioButton radioSeleccionProfesor; // Value injected by FXMLLoader
+
+    @FXML // fx:id="textoNotificaciones"
+    private TextArea textoNotificaciones; // Value injected by FXMLLoader
+
+    @FXML // fx:id="textoResultados"
+    private TextArea textoResultados; // Value injected by FXMLLoader
+
 }
