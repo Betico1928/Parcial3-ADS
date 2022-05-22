ba713a6149875d108d68660d4b40ce85448b0ce9
Betico1928 <betoveluis@gmail.com>
2022-05-21 20:46:49 -0500

    Inicio de la Integración

--- src/main/java/com/example/parcial_3ads/main.java
+++ src/main/java/com/example/parcial_3ads/main.java
@@ -12,27 +12,27 @@ public class main {
         
         Nomina nomina_01 = new Nomina () ;
         
-        ControladorArchivosNomina.leerNomina(nomina_01.getEmpleados_ListaCompleta(),"G:\\1. PCs\\1. inASUS_TUF\\1. Documents\\PUJ\\Java\\IV\\ADS\\Parcial_3\\Nomina.txt");
+        ControladorArchivosNomina.leerNomina(nomina_01.getEmpleados_ListaCompleta(),"/Users/case/IdeaProjects/Parcial3-ADS/Nomina.txt");
         
-//        for ( Empleado emp : nomina_01.getEmpleados_ListaCompleta() ) {
-//            
-//            if ( emp instanceof Profesor ) {
-//                
-//                System.out.println(emp.toString() ) ;
-//                System.out.println(( (Profesor)emp ) . getListaAsignaturas() ) ;
-//                
-//            } else if ( emp instanceof Monitor ) {
-//                
-//                System.out.println(emp.toString() ) ;
-//                System.out.println(( (Monitor)emp ) . getListaAsignaturas() ) ;
-//            
-//            } else if ( emp instanceof Empleado ) {
-//                
-//                System.out.println( emp.toString() );
-//            
-//            }
-//            
-//        }
+        for ( Empleado emp : nomina_01.getEmpleados_ListaCompleta() ) {
+
+            if ( emp instanceof Profesor ) {
+
+                System.out.println(emp.toString() ) ;
+                System.out.println(( (Profesor)emp ) . getListaAsignaturas() ) ;
+
+            } else if ( emp instanceof Monitor ) {
+
+                System.out.println(emp.toString() ) ;
+                System.out.println(( (Monitor)emp ) . getListaAsignaturas() ) ;
+
+            } else if ( emp instanceof Empleado ) {
+
+                System.out.println( emp.toString() );
+
+            }
+
+        }
         
     }
 
