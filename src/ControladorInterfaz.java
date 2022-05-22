3088f847d5a5eee3744a38d444a8385e930c215a
Jhoseph Lizarazo <jhosephslizarazom@outlook.com>
2022-05-22 14:04:44 -0500

    Commit de cambios para seguir enlazando la interfaz con el modelo del mundo.

---
+++ src/ControladorInterfaz.java
@@ -0,0 +1,159 @@
+
+package parcial_03_withfx;
+
+
+// Importaciones generales.
+import java.io.File;
+
+// Importaciones específicas para JavaFX.
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.FileChooser;
+
+
+
+public class ControladorInterfaz {
+    
+    
+    @FXML // fx:id="botonInscribirMateria"
+    private Button botonInscribirMateria; // Value injected by FXMLLoader
+
+    @FXML // fx:id="botonSeleccionNomina"
+    private Button botonSeleccionNomina; // Value injected by FXMLLoader
+
+    @FXML // fx:id="comboAsignarHoras"
+    private ComboBox<String> comboAsignarHoras; // Value injected by FXMLLoader
+    
+    @FXML // fx:id="comboAsignarMateria"
+    private ComboBox<String> comboAsignarMateria; // Value injected by FXMLLoader
+
+    @FXML // fx:id="radioSeleccionMonitor"
+    private RadioButton radioSeleccionMonitor; // Value injected by FXMLLoader
+
+    @FXML // fx:id="radioSeleccionProfesor"
+    private RadioButton radioSeleccionProfesor; // Value injected by FXMLLoader
+
+    @FXML // fx:id="textoNotificaciones"
+    private TextArea textoNotificaciones; // Value injected by FXMLLoader
+
+    @FXML // fx:id="textoResultados"
+    private TextArea textoResultados; // Value injected by FXMLLoader
+
+    @FXML
+    private Label AbsolutePathNomina;
+
+    @FXML
+    private TextField textIDEmpleado;
+
+    @FXML
+    void tomarPathDeArchivo(ActionEvent event) {
+        
+        // Apertura del Archivo Nomina
+        String rutaDelArchivo = null;
+
+        FileChooser SeleccionadorArchivo = new FileChooser();
+        
+        SeleccionadorArchivo.setTitle("Tomar el path absoluto");
+
+        File archivoSeleccionado = SeleccionadorArchivo.showOpenDialog(null);
+
+        if (archivoSeleccionado != null) {
+            
+            AbsolutePathNomina.setText(archivoSeleccionado.getAbsolutePath());
+
+            rutaDelArchivo = AbsolutePathNomina.getText();
+
+//            System.out.println(rutaDelArchivo);
+            
+            ControladorArchivosNomina.leerNomina(Nomina.getEmpleados_ListaCompleta(), rutaDelArchivo);
+
+            // Imprimir la nómina.
+//            for ( Empleado emp : nomina_01.getEmpleados_ListaCompleta() )
+//            {
+//                if ( emp instanceof Profesor )
+//                {
+//                    System.out.println(emp.toString() ) ;
+//                    System.out.println(( (Profesor)emp ) . getListaAsignaturas() ) ;
+//                }
+//                else if ( emp instanceof Monitor )
+//                {
+//                    System.out.println(emp.toString() ) ;
+//                    System.out.println(( (Monitor)emp ) . getListaAsignaturas() ) ;
+//                }
+//                else if ( emp instanceof Empleado )
+//                {
+//                    System.out.println( emp.toString() );
+//                }
+//            }
+            
+        } else {
+            System.out.println("Lo sentimos, no se ha podido abrir el archivo D:") ;
+        }
+
+
+        // Ingresar los datos en el ComboBox de Materias
+        ObservableList<String> listaDeMaterias = FXCollections.observableArrayList("Introduccion a la Programacion","Programacion Avanzada","Pensamiento Sistemico",
+                                                                                    "Comunicaciones y Redes","Analisis y Diseño de Software","Bases de Datos","Introduccion a la Ingenieria"
+                                                                                    ,"Seguridad de la Informacion","Gestion Financiera","CDIO","Arquitectura y Organizacion del Computador"
+                                                                                    ,"Sistemas Operativos","Sistemas Distribuidos","Desarrollo Web","Estructuras de Datos","Teoria de la Computacion") ;
+        
+        comboAsignarMateria.setItems(listaDeMaterias) ;
+
+        // Ingresar los datos en el ComboBox de Materias
+        ObservableList<String> listaDeHoras = FXCollections.observableArrayList("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20") ;
+        comboAsignarHoras.setItems(listaDeHoras) ;
+        
+    }
+
+    @FXML
+    void inscribirMateria (ActionEvent event) {
+        
+        // Para obtener la materia seleccionada en el combobox de AsignarMateria
+        String seleccionMaterias = comboAsignarMateria.getSelectionModel().getSelectedItem() ;
+
+        // Para obtener el numero de horas seleccionadas en el combobox de AsignarHoras
+        String seleccionHoras = comboAsignarHoras.getSelectionModel().getSelectedItem() ;
+
+        // Para obtener el tipo de empleado al cual se le va a incribir la materia
+        String tipoEmpleado = seleccionarTipoEmpleado(event) ;
+
+        //Para obetener el ID del empleado:
+        String idEmpleado = textIDEmpleado.getText() ;
+
+        // Impresion de todos los datos obtenidos:
+        System.out.println("La materia seleccionada es: " + seleccionMaterias) ;
+        System.out.println("La cantidad de horas que se van a inscribir es de: " + seleccionHoras) ;
+        System.out.println("El tipo de empleado es: " + tipoEmpleado) ;
+        System.out.println("El nombre del empleado es: " + idEmpleado) ;
+
+        double seleccionHorasDouble = Double.parseDouble(seleccionHoras) ;
+        
+        Nomina.aniadirAsignatura_A_Empleado(seleccionMaterias ,seleccionHorasDouble ,idEmpleado) ;
+        
+    }
+
+    @FXML
+    String seleccionarTipoEmpleado (ActionEvent event) {
+        
+        String tipoEmpleado = null ;
+
+        if (radioSeleccionProfesor.isSelected()) {
+            
+            tipoEmpleado = "Profesor" ;
+            
+        }
+
+        else if (radioSeleccionMonitor.isSelected()) {
+            
+            tipoEmpleado = "Monitor" ;
+            
+        }
+        
+    return tipoEmpleado;
+    }
+    
+    
+}
